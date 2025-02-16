b20c5a66f3c6e91ae8c2329eb548b6d469ba7899
Connor Turetzky <cturetzky@gmail.com>
2025-02-16 13:18:40 -0800

    Added updates to request handler

--- src/sar_project/agents/sar_agent_rtl.py
+++ src/sar_project/agents/sar_agent_rtl.py
@@ -12,8 +12,9 @@ class RTLAgent(SARBaseAgent):
         self.team_members = []
         self.current_operation_status = {}
         self.team_status = {}
+        self.op_chief = {}
 
-    def process_request(self, message):
+    def process_request(self, message, status=None, member=None):
         """process Team based requests"""
         try:
             if "get_team_members" in message:
@@ -22,28 +23,48 @@ class RTLAgent(SARBaseAgent):
                 return self.get_op_status()
             elif "get_team_status" in message:
                 return self.get_team_status()
+            elif "clear_op" in message:
+                self.clear_op()
+                return "Operation Cleared"
+            elif "new_op" in message:
+                self.new_op()
+                return "Operation Created; Previous Operation Cleared"
+            elif "update_team_status" in message:
+                self.update_team_status(self, status)
+            elif "update_current_op_status" in message:
+                self.update_current_op_status(self, status)
+            elif "add_team_member" in message:
+                self.add_team_member(self, member)
+
         except Exception as e:
             return {"error":str(e)}
 
     def clear_op(self):
-        self.update_current_op_status("Off-duty")
-        self.update_team_status("Off-duty")
+        self.update_current_op_status("Off-Duty")
+        self.update_team_status("Off-Duty")
+        self.notify_team("Operation is cleared. You are officially Off-Duty.")
         self.team_members = []
     def new_op(self, team_members):
         self.clear_op()
         self.update_current_op_status("New Operation: Planning")
         for member in team_members:
             self.add_team_member(member)
+        self.notify_team(self, "New Operation! Please stand by.")
 
     def update_current_op_status(self, status):
         self.current_operation_status = status
     def update_team_status(self, status):
         self.team_status = status
+        self.notify_team("Team status updated: " + status)
     def add_team_member(self, rescuer):
         self.team_members.append(rescuer)
+        self.notify_team("New Team Member added: " + rescuer)
     def get_team_members(self):
         return getattr(self, "team_members", "Unknown")
     def get_op_status(self):
         return getattr(self, "current_operation_status", "Unknown")
     def get_team_status(self):
         return getattr(self, "team_status", "Unknown")
+
+    def notify_team(self, message):
+        return("Team Notified")
